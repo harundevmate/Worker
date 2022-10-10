c2988ccbeee57207511c271e7c0e574b9a6c4edc
lukman harun <lukman.harun@nttindonesia.com>
2022-10-10 22:28:17 +0700

    Add project files.

---
+++ Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+//using Grpc.Net.Client;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task Get()
+        {
+            // The port number must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("http://localhost:5271");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Worker/Services/GreeterService.cs
@@ -0,0 +1,41 @@
+using Grpc.Core;
+using Grpc.Net.Client;
+using Worker;
+
+namespace Worker.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        public GreeterService()
+        {
+        }
+        public override async Task<ResponseMessage> BroadCast(RequestMessage request, ServerCallContext context)
+        {
+
+            // The port number must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("http://localhost:5271");
+            var client = new Greeter.GreeterClient(channel);
+            request.Name = "Worker";
+            var send = await client.ReceiverAsync
+                (
+                    request
+                );
+            Console.WriteLine(request.Name + ":" + request.Body);
+
+            return await Task.FromResult(new ResponseMessage
+            {
+                Name = request.Name,
+                Body = request.Body
+            });
+        }
+        public override Task<ResponseMessage> Receiver(RequestMessage request, ServerCallContext context)
+        {
+            Console.WriteLine("From " + request.Name + ":" + request.Body);
+            return Task.FromResult(new ResponseMessage
+            {
+                Name = request.Name,
+                Body = request.Body
+            });
+        }
+    }
+}
